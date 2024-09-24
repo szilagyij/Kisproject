cde988ec331bed1a9c8ca2bc2c8caeaad1046d32
SzilagyiJ <112682509+szilagyij@users.noreply.github.com>
2024-09-24 08:02:16 +0200

    mfinal

--- js.js
+++ js.js
@@ -1,13 +1,11 @@
-
-function AgeSlider(){
+// Életkor frissítése
+function AgeSlider() {
     const ageSlider = document.getElementById('age');
     const ageLabel = document.getElementById('ageLabel');
-    ageSlider.addEventListener('input', () => {
-        ageLabel.innerText = ageSlider.value;
-    });
-
+    ageLabel.innerText = ageSlider.value;
 }
-let isSecondClick = false;
+
+// Jelszó generálás
 function PasswordGenerate() {
     const generatePasswordButton = document.getElementById('generatePassword');
     generatePasswordButton.addEventListener('click', () => {
@@ -23,48 +21,10 @@ function PasswordGenerate() {
 
 PasswordGenerate();
 
-function RegBtn() {
-        const name = document.getElementById('name');
-        const email = document.getElementById('email');
-        const profession = document.getElementById('profession');
-        const age = document.getElementById('age');
-        const password = document.getElementById('password');
+document.getElementById('registrationForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const formData = new FormData(this);
+    const data = Object.fromEntries(formData.entries());
+    console.log(data);
 
-        if (name.value === '') {
-            name.style.borderColor = 'red';
-        } else {
-            name.style.borderColor = '';
-        }
-
-        if (email.value === '') {
-            email.style.borderColor = 'red';
-        } else {
-            email.style.borderColor = '';
-        }
-
-        if (profession.value === '') {
-            profession.style.borderColor = 'red';
-        } else {
-            profession.style.borderColor = '';
-        }
-
-        if (age.value === '') {
-            age.style.borderColor = 'red';
-        } else {
-            age.style.borderColor = '';
-        }
-
-        if (password.value === '') {
-            password.style.borderColor = 'red';
-        } else {
-            password.style.borderColor = '';
-        }
-
-        if (name.value !== '' && email.value !== '' && profession.value !== '' && age.value !== '' && password.value !== '') {
-            console.log(name.value, email.value, profession.value, age.value, password.value);
-        } else {
-            alert('Kérlek töltse ki az összes mezőt!');
-        }
-    
-}
-
+});
